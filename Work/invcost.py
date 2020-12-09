0f8a968d58aa5a352a7d226f63d06cf60ef096e1
Madhuchigri <55974223+Madhuchigri@users.noreply.github.com>
2020-12-09 21:28:45 +0530

    Add files via upload

---
+++ Work/invcost.py
@@ -0,0 +1,12 @@
+def inventory():
+    with open("inventory.csv") as FH:
+        headers = next(FH)
+        total = 0.0
+        for line in FH:
+           # print(line)
+            parts = line.split(',')
+            val = int(parts[1]) * float(parts[2])
+            total += val
+        print("The total amount of the inventory is", total)
+
+inventory()
