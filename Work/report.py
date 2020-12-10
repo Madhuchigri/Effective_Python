3d3c59651fe6114dfe2ba6d618f17f85eb070e03
Madhuchigri <55974223+Madhuchigri@users.noreply.github.com>
2020-12-10 21:01:05 +0530

    Add files via upload

---
+++ Work/report.py
@@ -0,0 +1,17 @@
+
+import csv
+def read_inventory(filename):
+    with open(filename, 'rt') as FH:
+        rows = csv.reader(FH)  # using ssv.reader method to read the lines..no need of split
+        headers = next(rows)
+        product = []
+        for row in rows:
+            prod = (row[0], int(row[1]), float(row[2]))
+           # print(tuple1)
+            product.append(prod)
+        return product
+
+filename = "inventory.csv"
+
+report = read_inventory(filename)
+print(report)
