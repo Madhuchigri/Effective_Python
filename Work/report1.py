88bd04979670c69861c89818070105f1b01520df
Madhuchigri <55974223+Madhuchigri@users.noreply.github.com>
2020-12-14 21:29:18 +0530

    Add files via upload

---
+++ Work/report1.py
@@ -0,0 +1,51 @@
+import csv
+
+
+def read_inventory(filename):
+    with open(filename, 'rt') as FH:
+        rows = csv.reader(FH)  # using ssv.reader method to read the lines..no need of split
+        headers = next(rows)
+        product = []
+        for row in rows:
+            prod = {'name':row[0], 'quant':int(row[1]), 'price':float(row[2])}
+            product.append(prod)
+        return product
+
+
+
+def read_prices(filename1):
+    with open(filename1, 'rt') as FH:
+        prices = dict()
+        rows = csv.reader(FH)  # using ssv.reader method to read the lines..no need of split
+        for row in rows:
+           try:
+            prices[row[0]] = float(row[1])
+           except IndexError:
+               continue   # Empty row found so continuing with the execution
+    return prices
+
+
+def make_report(product,prices):
+    values = list()
+    for prod in product:
+        name = prod['name']
+        quant = prod['quant']
+        price = prod['price']
+        latest_price = prices[name]
+        values.append ((name,quant,latest_price,latest_price-price))
+    return values
+
+
+filename = "inventory.csv"
+filename1 = "prices.csv"
+
+report = read_inventory(filename)
+price_file = read_prices(filename1)
+report1 = make_report(report,price_file)
+headers = ('Name','Quantity','prices','Change')
+print('%10s %10s %10s %10s'%headers)
+width = 10
+n_cols = len(headers)
+print(f"{'-'*width} " * n_cols)
+for name,quant,price,change in report1 :
+    print(f'{name:>10s}{quant:>10d}{price:10.2f}{change:>10.2f}')
